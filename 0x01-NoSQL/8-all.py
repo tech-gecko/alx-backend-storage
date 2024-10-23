33d1da174229f53ba63fe0158ef30ae5e1e89aa4
tech-gecko <w0nderzz2001@gmail.com>
2024-10-23 11:15:47 -0700

    Update tasks 8 and 9

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 """
-    'list_all's module.
+    Module containing the 'list_all' function.
 """
 
+
+def list_all(mongo_collection):
+    """
+        Function that lists all documents in a collection.
+        Returns an empty list if no document is in the collection.
+        'mongo_collection' will be the pymongo collection object.
+    """
+    documents = mongo_collection.find()
+
+    if not documents:
+        return []
+
+    return list(documents)
+
+
 if __name__ == "__main__":
-    def list_all(mongo_collection):
-        """
-            Function that lists all documents in a collection.
-            Returns an empty list if no document is in the collection.
-            'mongo_collection' will be the pymongo collection object.
-        """
-        documents = mongo_collection.all()
-
-        if not documents:
-            return []
-
-        return list(documents)
+    pass
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-""" 'insert_school's module."""
+"""
+    Module containing the 'insert_school' function.
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+        Function that inserts a new document in a collection
+        based on kwargs.
+        'mongo_collection' will be the pymongo collection object.
+        Returns the new '_id'.
+    """
+    _id = mongo_collection.insert_one(kwargs)
+
+    return _id
+
 
 if __name__ == "__main__":
-    def insert_school(mongo_collection, **kwargs):
-        """
-            Function that inserts a new document in a collection
-            based on kwargs.
-            'mongo_collection' will be the pymongo collection object.
-            Returns the new '_id'.
-        """
-        new_doc = []
-        new_doc.append(**kwargs)
-
-        mongo_collection.insert(new_doc)
-
-        return mongo_collection._id
+    pass
