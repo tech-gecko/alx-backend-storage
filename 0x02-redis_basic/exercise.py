385cb089dd1f5c681798d75ba85af2cf96da6a51
tech-gecko <w0nderzz2001@gmail.com>
2024-10-25 21:49:58 +0000

    Add task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Optional, Union
 
 
 class Cache:
@@ -23,3 +23,22 @@ class Cache:
         self._redis.set(random_key, data)
 
         return random_key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+        """Retrieve the stored data, using an optional callable `fn` for conversion."""
+        value = self._redis.get(key)
+
+        if not value:
+            return None
+        if fn:
+            return fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Retrieve data as a string."""
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Retrieve data as an integer."""
+        return self.get(key, fn=int)
