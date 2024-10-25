ad4c8a1802890d86ad0447f34ca57a01e6009f88
tech-gecko <w0nderzz2001@gmail.com>
2024-10-25 21:08:14 +0000

    Try removing the indentation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,9 +18,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(
-            self,
-            data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
 
